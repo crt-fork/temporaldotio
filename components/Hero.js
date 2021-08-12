75090599aabb93a37bc197c04407494139ed0fc3
m-abdelwahab <hey@mahmoud.codes>
2021-08-12 23:20:26 +0200

    fix link

--- components/Hero.js
+++ components/Hero.js
@@ -20,7 +20,7 @@ export default function Hero() {
             orchestration for mission-critical applications.
           </p>
           <div className="py-4 text-lg font-medium flex flex-col justify-center sm:justify-start space-y-4 sm:space-y-0 sm:flex-row sm:space-x-4 items-center">
-            <DirectionalControl href="#explain-temporal">Get Started</DirectionalControl>
+            <DirectionalControl href="https://docs.temporal.io/">Get Started</DirectionalControl>
             <DirectionalControl secondary={true} href="#explain-temporal">
               How it works
             </DirectionalControl>
