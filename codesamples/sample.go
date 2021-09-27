77e946dc75efa07d03005654132ed7ab19f74674
sw-yx <shawnthe1@gmail.com>
2021-09-27 14:30:50 -0700

    fix go example

--- codesamples/sample.go
+++ codesamples/sample.go
@@ -1,8 +1,9 @@
-func RemindUserWorkflow(ctx workflow.Context, userId string, intervals []int) error {
+func RemindUserWorkflow(ctx workflow.Context, userID string, intervals []int) error {
   // Send reminder emails, e.g. after 1, 7, and 30 days
-  for interval, _ := range intervals {
-    _ = workflow.Sleep(interval * time.Day)                            // Sleep for days!
-    _ = workflow.ExecuteActivity(ctx, SendEmail, userId).Get(ctx, nil) // Activities are retried by default!
+  for _, interval := range intervals {
+    _ = workflow.Sleep(ctx, days(interval)) // Sleep for days!
+    _ = workflow.ExecuteActivity(ctx, SendEmail, userID).Get(ctx, nil) 
+    // Activities have timeouts, and will be retried by default!
   }
-  // Easily cancelled when user unsubscribes
+  // ...
 }
--- components/CodeSwitcher/Go.js
+++ components/CodeSwitcher/Go.js
@@ -9,120 +9,101 @@ export const Go = () => {
       className="p-4 rounded-br-lg rounded-bl-lg overflow-auto line-numbers
     ">
       <code className="overflow">
-        <p>
-          <span style={{ color: '#89DDFF' }}>func</span>
-          <span style={{ color: '#A6ACCD' }}> </span>
-          <span style={{ color: '#82AAFF' }}>RemindUserWorkflow</span>
-          <span style={{ color: '#89DDFF' }}>(</span>
-          <span style={{ color: '#A6ACCD' }}>ctx workflow</span>
-          <span style={{ color: '#89DDFF' }}>.</span>
-          <span style={{ color: '#A6ACCD' }}>Context</span>
-          <span style={{ color: '#89DDFF' }}>,</span>
-          <span style={{ color: '#A6ACCD' }}> userID </span>
-          <span style={{ color: '#C792EA' }}>string</span>
-          <span style={{ color: '#89DDFF' }}>,</span>
-          <span style={{ color: '#A6ACCD' }}> intervals </span>
-          <span style={{ color: '#89DDFF' }}>[]</span>
-          <span style={{ color: '#C792EA' }}>int</span>
-          <span style={{ color: '#89DDFF' }}>)</span>
-          <span style={{ color: '#A6ACCD' }}> </span>
-          <span style={{ color: '#C792EA' }}>error</span>
-          <span style={{ color: '#A6ACCD' }}> </span>
-          <span style={{ color: '#89DDFF' }}>{'{'}</span>
-        </p>
-        <p>
-          <span style={{ color: '#A6ACCD' }}>{'  '}</span>
-          <span style={{ color: '#676E95', fontStyle: 'italic' }}>//</span>
-          <span style={{ color: '#90EE90', fontStyle: 'italic' }}>
-            {' '}
-            Send reminder emails, e.g. after 1, 7, and 30 days
+        <span style="display:block">
+          <span style="color: #89DDFF">func</span>
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #82AAFF">RemindUserWorkflow</span>
+          <span style="color: #89DDFF">(</span>
+          <span style="color: #A6ACCD">ctx workflow</span>
+          <span style="color: #89DDFF">.</span>
+          <span style="color: #A6ACCD">Context</span>
+          <span style="color: #89DDFF">,</span>
+          <span style="color: #A6ACCD"> userID </span>
+          <span style="color: #C792EA">string</span>
+          <span style="color: #89DDFF">,</span>
+          <span style="color: #A6ACCD"> intervals </span>
+          <span style="color: #89DDFF">[]</span>
+          <span style="color: #C792EA">int</span>
+          <span style="color: #89DDFF">)</span>
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #C792EA">error</span>
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #89DDFF">{'{'}</span>
+        </span>
+        <span style="display:block">
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #90EE90; font-style: italic">
+            // Send reminder emails, e.g. after 1, 7, and 30 days
           </span>
-        </p>
-        <p>
-          <span style={{ color: '#A6ACCD' }}>{'  '}</span>
-          <span style={{ color: '#89DDFF', fontStyle: 'italic' }}>for</span>
-          <span style={{ color: '#A6ACCD' }}> _</span>
-          <span style={{ color: '#89DDFF' }}>,</span>
-          <span style={{ color: '#A6ACCD' }}> interval </span>
-          <span style={{ color: '#89DDFF' }}>:=</span>
-          <span style={{ color: '#A6ACCD' }}> </span>
-          <span style={{ color: '#89DDFF', fontStyle: 'italic' }}>range</span>
-          <span style={{ color: '#A6ACCD' }}> intervals </span>
-          <span style={{ color: '#89DDFF' }}>{'{'}</span>
-        </p>
-        <p>
-          <span style={{ color: '#A6ACCD' }}>{'    '}_ </span>
-          <span style={{ color: '#89DDFF' }}>=</span>
-          <div className="border-b-2 border-dotted border-offwhite inline cursor-pointer">
-            <Tippy
-              arrow={true}
-              placement="left"
-              theme="dark"
-              content="This function can sleep for days then the workflow will continue execution!">
-              <div className="inline">
-                <span style={{ color: '#A6ACCD' }}> workflow</span>
-                <span style={{ color: '#89DDFF' }}>.</span>
-                <span style={{ color: '#82AAFF' }}>Sleep</span>
-                <span style={{ color: '#89DDFF' }}>(</span>
-                <span style={{ color: '#A6ACCD' }}>interval </span>
-                <span style={{ color: '#89DDFF' }}>*</span>
-                <span style={{ color: '#A6ACCD' }}> 24 </span>
-                <span style={{ color: '#89DDFF' }}>*</span>
-                <span style={{ color: '#A6ACCD' }}> time</span>
-                <span style={{ color: '#89DDFF' }}>.</span>
-                <span style={{ color: '#A6ACCD' }}>Hour</span>
-                <span style={{ color: '#89DDFF' }}>)</span>
-              </div>
-            </Tippy>
-          </div>
-          <span style={{ color: '#A6ACCD' }}>{'                            '}</span>
-        </p>
-        <p>
-          <span style={{ color: '#A6ACCD' }}>{'    '}_ </span>
-          <span style={{ color: '#89DDFF' }}>=</span>
-          <div className="border-b-2 border-dotted border-offwhite inline cursor-pointer">
-            <Tippy
-              arrow={true}
-              placement="left"
-              theme="dark"
-              content="Activities are retried by default!">
-              <div className="inline">
-                <span style={{ color: '#A6ACCD' }}> </span>
-                <span style={{ color: '#A6ACCD' }}>workflow</span>
-                <span style={{ color: '#89DDFF' }}>.</span>
-                <span style={{ color: '#82AAFF' }}>ExecuteActivity</span>
-                <span style={{ color: '#89DDFF' }}>(</span>
-                <span style={{ color: '#A6ACCD' }}>ctx</span>
-                <span style={{ color: '#89DDFF' }}>,</span>
-                <span style={{ color: '#A6ACCD' }}> SendEmail</span>
-                <span style={{ color: '#89DDFF' }}>,</span>
-                <span style={{ color: '#A6ACCD' }}> userID</span>
-                <span style={{ color: '#89DDFF' }}>).</span>
-                <span style={{ color: '#82AAFF' }}>Get</span>
-                <span style={{ color: '#89DDFF' }}>(</span>
-                <span style={{ color: '#A6ACCD' }}>ctx</span>
-                <span style={{ color: '#89DDFF' }}>,</span>
-                <span style={{ color: '#A6ACCD' }}> </span>
-                <span style={{ color: '#89DDFF' }}>nil)</span>
-              </div>
-            </Tippy>
-          </div>
-        </p>
-        <p>
-          <span style={{ color: '#A6ACCD' }}>{'  '}</span>
-          <span style={{ color: '#89DDFF' }}>{'}'}</span>
-        </p>
-        <p>
-          <span style={{ color: '#A6ACCD' }}>{'  '}</span>
-          <span style={{ color: '#676E95', fontStyle: 'italic' }}>//</span>
-          <span style={{ color: '#90EE90', fontStyle: 'italic' }}>
-            {' '}
-            Easily cancelled when user unsubscribes
+        </span>
+        <span style="display:block">
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #89DDFF; font-style: italic">for</span>
+          <span style="color: #A6ACCD"> _</span>
+          <span style="color: #89DDFF">,</span>
+          <span style="color: #A6ACCD"> interval </span>
+          <span style="color: #89DDFF">:=</span>
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #89DDFF; font-style: italic">range</span>
+          <span style="color: #A6ACCD"> intervals </span>
+          <span style="color: #89DDFF">{'{'}</span>
+        </span>
+        <span style="display:block">
+          <span style="color: #A6ACCD">{'    '}_ </span>
+          <span style="color: #89DDFF">=</span>
+          <span style="color: #A6ACCD"> workflow</span>
+          <span style="color: #89DDFF">.</span>
+          <span style="color: #82AAFF">Sleep</span>
+          <span style="color: #89DDFF">(</span>
+          <span style="color: #A6ACCD">ctx</span>
+          <span style="color: #89DDFF">,</span>
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #82AAFF">days</span>
+          <span style="color: #89DDFF">(</span>
+          <span style="color: #A6ACCD">interval</span>
+          <span style="color: #89DDFF">))</span>
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #90EE90; font-style: italic">// Sleep for days!</span>
+        </span>
+        <span style="display:block">
+          <span style="color: #A6ACCD">{'    '}_ </span>
+          <span style="color: #89DDFF">=</span>
+          <span style="color: #A6ACCD"> workflow</span>
+          <span style="color: #89DDFF">.</span>
+          <span style="color: #82AAFF">ExecuteActivity</span>
+          <span style="color: #89DDFF">(</span>
+          <span style="color: #A6ACCD">ctx</span>
+          <span style="color: #89DDFF">,</span>
+          <span style="color: #A6ACCD"> SendEmail</span>
+          <span style="color: #89DDFF">,</span>
+          <span style="color: #A6ACCD"> userID</span>
+          <span style="color: #89DDFF">).</span>
+          <span style="color: #82AAFF">Get</span>
+          <span style="color: #89DDFF">(</span>
+          <span style="color: #A6ACCD">ctx</span>
+          <span style="color: #89DDFF">,</span>
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #89DDFF">nil)</span>
+          <span style="color: #A6ACCD"> </span>
+        </span>
+        <span style="display:block">
+          <span style="color: #A6ACCD">{'    '}</span>
+          <span style="color: #90EE90; font-style: italic">
+            // Activities have timeouts, and will be retried by default!
           </span>
-        </p>
-        <p>
-          <span style={{ color: '#89DDFF' }}>{'}'}</span>
-        </p>
+        </span>
+        <span style="display:block">
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #89DDFF">{'}'}</span>
+        </span>
+        <span style="display:block">
+          <span style="color: #A6ACCD"> </span>
+          <span style="color: #90EE90; font-style: italic">// ...</span>
+        </span>
+        <span style="display:block">
+          <span style="color: #89DDFF">{'}'}</span>
+        </span>
+        <span style="display:block"></span>
       </code>
     </pre>
   );
