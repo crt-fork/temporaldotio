c7d6ca075b855639ccdc84daa225781d1f688bcd
Roey Berman <roey.berman@gmail.com>
2021-10-21 10:06:43 -0400

    Update sample.js

--- codesamples/sample.js
+++ codesamples/sample.js
@@ -1,7 +1,7 @@
 async function remindUserWorkflow(userId: string, intervals: number[]) {
   // Send reminder emails, e.g. after 1, 7, and 30 days
   for (const interval of intervals) {
-    await sleep(interval * DAYS); // Sleep for days!
+    await sleep(`${interval} days`); // Sleep for days!
     await activities.sendEmail(interval, userId); // Activities retried by default!
   }
   // Easily cancelled when user unsubscribes
