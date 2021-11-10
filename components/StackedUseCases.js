389d989a3bcbe4f797a2e0d43b407abf7f79203b
m-abdelwahab <hey@mahmoud.codes>
2021-11-10 15:29:04 +0200

    update usecases section

--- components/StackedUseCases.js
+++ components/StackedUseCases.js
@@ -1,14 +1,17 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import DirectionalControl from './DirectionalControl';
 
-const Stack = ({ content, link, title }) => (
+const Stack = ({ content, link, title, icon }) => (
   <div className="stack after:transition-all  relative">
     <Link href={link}>
       <a>
-        <div className="stack__content transition-all h-full w-full p-4 shadow-lg rounded-lg relative flex flex-col">
-          <h2 className="text-2xl tracking-wide font-bold">{title}</h2>
+        <div className="stack__content transition-all h-full w-full rounded-lg relative flex flex-col items-start">
+          <div className="mb-3 rounded-lg bg-blue-300 p-2 w-12 h-12">
+            <Image width="60" height="60" src={icon} alt={`${title} icon`} />
+          </div>
+          <h2 className="text-2xl tracking-wide font-semibold">{title}</h2>
           <p className="flex-grow font-light my-4 text-lg">{content}</p>
-          <a className="nav-link text-center ">Learn More</a>
         </div>
       </a>
     </Link>
@@ -17,6 +20,7 @@ const Stack = ({ content, link, title }) => (
 
 const USE_CASES = [
   {
+    icon: '/icons/icon-sharding.svg',
     link: '/usecases#orchestration',
     content: (
       <div>
@@ -27,6 +31,7 @@ const USE_CASES = [
     title: 'Microservices Orchestration'
   },
   {
+    icon: '/icons/icon-sharding.svg',
     content: (
       <div>
         <span className="">
@@ -40,6 +45,7 @@ const USE_CASES = [
     title: 'Financial Transactions'
   },
   {
+    icon: '/icons/icon-streaming.svg',
     content: (
       <div>
         When ad-hoc provisioning doesn't scale and Kubernetes or Fargate doesn't give you enough
@@ -50,6 +56,7 @@ const USE_CASES = [
     title: 'Resource Provisioning'
   },
   {
+    icon: '/icons/icon-storage.svg',
     content: (
       <div>
         Reliable polling that is as simple or sophisticated as you need, at massive scale. Imagine{' '}
@@ -59,32 +66,64 @@ const USE_CASES = [
     ),
     link: '/usecases#monitoring',
     title: 'Monitoring'
+  },
+  {
+    icon: '/icons/icon-storage.svg',
+    content: (
+      <div>
+        Reliable polling that is as simple or sophisticated as you need, at massive scale. Imagine{' '}
+        <span className="">100s of millions parallel cron jobs</span> that survive any machine
+        failure.
+      </div>
+    ),
+    link: '/usecases#monitoring',
+    title: 'Long Running Processes'
+  },
+  {
+    icon: '/icons/icon-storage.svg',
+    content: (
+      <div>
+        Reliable polling that is as simple or sophisticated as you need, at massive scale. Imagine{' '}
+        <span className="">100s of millions parallel cron jobs</span> that survive any machine
+        failure.
+      </div>
+    ),
+    link: '/usecases#monitoring',
+    title: 'Distributed Transactions'
   }
 ];
 
 const StackedUseCases = () => (
-  <section id="use-cases" className="bg-gray-50">
-    <div
-      className={`
+  <>
+    <section id="use-cases" className="bg-gray-800 bg-svg">
+      <div
+        className={`
     container mx-auto
     px-8 py-36
     `}>
-      <h2 className="text-center text-3xl font-bold sm:text-6xl mb-16">Use Cases</h2>
-      <div className="grid gap-10 md:grid-cols-2 lg:w-800 mx-auto">
-        {USE_CASES.map((useCase) => (
-          <Stack
-            content={useCase.content}
-            link={useCase.link}
-            title={useCase.title}
-            key={useCase.link}
-          />
-        ))}
+        <h3 className="text-4xl md:text-5xl font-bold mb-20">
+          Simple building blocks,
+          <br /> hundreds of uses cases
+        </h3>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-12  mx-auto">
+          {USE_CASES.map((useCase) => (
+            <Stack
+              icon={useCase.icon}
+              content={useCase.content}
+              link={useCase.link}
+              title={useCase.title}
+              key={useCase.link}
+            />
+          ))}
+        </div>
+        <div className="mt-16 flex justify-center">
+          <DirectionalControl secondary href="/usecases">
+            Learn more
+          </DirectionalControl>
+        </div>
       </div>
-      <div className="mt-16 flex justify-center">
-        <DirectionalControl href="/usecases">More Use Cases</DirectionalControl>
-      </div>
-    </div>
-  </section>
+    </section>
+  </>
 );
 
 export default StackedUseCases;
