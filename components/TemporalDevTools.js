cf56aece0aeaac79eac6d84084b39435143bd586
m-abdelwahab <hey@mahmoud.codes>
2021-12-02 00:09:10 +0200

    add Temporal cluster and devtools section

--- components/TemporalDevTools.js
+++ components/TemporalDevTools.js
@@ -1,39 +1,32 @@
 import React from 'react';
-import Image from 'next/image';
-import DirectionalControl from './DirectionalControl';
 
 export const TemporalDevTools = () => {
   return (
     <>
-      <div className="bg-gray-1000">
-        <section className="max-w-screen-2xl mx-auto pt-4 pb-28 lg:ml-20 grid grid-cols-1 lg:grid-cols-12  items-center justify-center text-gray-100">
-          <div className="col-span-1 lg:col-span-7">
-            <Image
-              className="object-cover"
-              src="/temporal-system-tools.svg"
-              width="900"
-              height="600"
-              alt=""
+      <div className="p-8 rounded-lg bg-gray-700 max-w-xl">
+        <h2 className="text-xl mb-3 text-gray-50">Temporal DevTools</h2>
+        <h3 className="text-4xl font-bold mb-4 tracking-wide">Observability & Control</h3>
+        <p className="text-lg leading-relaxed mb-3">
+          Make the "distributed systems murder mystery" as predictable as Scooby Doo, with a Web UI
+          that provides end-to-end visibility and metadata search, and a CLI to query and administer
+          your Cluster!
+        </p>
+        <div className="text-blue-200 flex items-center space-x-2 mt-10 text-xl ">
+          <a className=" hover:underline" href="https://docs.temporal.io/docs/server/introduction">
+            Temporal DevTools docs
+          </a>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              fillRule="evenodd"
+              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
             />
-          </div>
-          <div className="col-span-1 md:col-span-5 px-8">
-            {/* <h2 className="text-4xl font-bold mb-4 tracking-wide">Temporal DevTools</h2> */}
-            <h2 className="text-xl mb-3 text-gray-50">Temporal DevTools</h2>
-            <h3 className="text-4xl font-bold mb-4 tracking-wide">Observability & Control</h3>
-            <div className="text-lg">
-              <p className="mb-3">
-                Make the "distributed systems murder mystery" as predictable as Scooby Doo, with a
-                Web UI that provides end-to-end visibility and metadata search, and a CLI to query
-                and administer your Cluster!
-              </p>
-            </div>
-            <DirectionalControl
-              className="w-48 mt-10"
-              href="https://docs.temporal.io/docs/system-tools/introduction">
-              Read docs
-            </DirectionalControl>
-          </div>
-        </section>
+          </svg>
+        </div>
       </div>
     </>
   );
