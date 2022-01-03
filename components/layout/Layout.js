5c74afa784da793cbb50e3d3307ddc4f77a6b4be
sw-yx <shawnthe1@gmail.com>
2022-01-04 00:34:46 +0800

    put back Banner

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Footer } from './Footer';
 import { Nav } from './Nav';
-// import { Banner } from './Banner';
+import { Banner } from './Banner';
 
 export const Layout = ({ children }) => {
   return (
     <div className="bg-gray-900 text-gray-100">
-      {/* <Banner />  temporarily comment out, no workshop */}
+      <Banner />
       <Nav />
       <main>{children}</main>
       <Footer />
